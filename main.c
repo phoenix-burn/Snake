7184724b8b86f480a397a8e425cc5cb0bf09e500
a.bazkhanei <a.bazkhanei@hpds.ir>
2022-09-05 11:18:52 +0430

    First

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+int main (int argc, char* argv[])
+{
+	if ( !(SDL_Init (SDL_INIT_EVERYTHING)) )
+	{
+
+	}
+}
